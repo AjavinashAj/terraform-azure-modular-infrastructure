18914a6f734ccb63933b5bc3c16e7a423230d20e
Avinah <avinash0000820@gmail.com>
2026-08-29 19:16:30 +0530

    xyz

--- environments/dev/provider.tf
+++ environments/dev/provider.tf
@@ -7,9 +7,7 @@ terraform {
   }
   backend "azurerm" {
     
-    tenant_id            = "b5442b50-7a5e-415c-ae67-fecb43704c91"  
-    subscription_id      = "c9716a7e-f1f8-4398-8421-9f293e9eef04"  
-    resource_group_name  = "EY-rg"          
+     resource_group_name  = "EY-rg"          
     storage_account_name = "eystorage01"                              
     container_name       = "eycontainer"                               
     key                  = "dev.eycontainer"               
