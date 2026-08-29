81b2f5207d15407218540a5defce88b05e5aa904
Avinah <avinash0000820@gmail.com>
2026-08-29 15:29:54 +0530

    update provider

--- environments/dev/provider.tf
+++ environments/dev/provider.tf
@@ -2,11 +2,20 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.8.0"
+      version = "5.0.0"
     }
+  }
+  backend "azurerm" {
+    
+    tenant_id            = "b5442b50-7a5e-415c-ae67-fecb43704c91"  
+    subscription_id      = "c9716a7e-f1f8-4398-8421-9f293e9eef04"  
+    resource_group_name  = "EY-rg"          
+    storage_account_name = "eystorage01"                              
+    container_name       = "eycontainer"                               
+    key                  = "dev.eycontainer"               
   }
 }
 provider "azurerm" {
   features {}
-  subscription_id = "03ae0d8b-56f1-4438-b3ce-c57dc4f35df6"
+  
 }
